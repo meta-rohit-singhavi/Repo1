bfdb9273a88acbe4641680b916143d2027183661
Rohit Singhavi <rohit.singhavi@metacube.com>
2016-07-26 17:50:27 +0530

    Update MenuMain.java

--- OOPS4/Menu_Submenu/MenuMain.java
+++ OOPS4/Menu_Submenu/MenuMain.java
@@ -7,25 +7,9 @@ public class MenuMain {
 	public static void main(String[] args) {
 
 		//Initializing menus and submenus
-		Menu menuTV= new Menu("TV"); 
-		menuTV.setSubMenu(new SubMenu("LED"));
-		menuTV.setSubMenu(new SubMenu("LCD"));
-		
-		Menu menuMobile= new Menu("Mobile");
-		menuMobile.setSubMenu(new SubMenu("IOS"));
-		menuMobile.setSubMenu(new SubMenu("Android"));
-		
-		Menu menuPC= new Menu("PC");
-		menuPC.setSubMenu(new SubMenu("Desktop"));
-		menuPC.setSubMenu(new SubMenu("Laptop"));
-		
-		MenuAction menuAction= new MenuAction();
-		menuAction.addMenu(menuTV);
-		menuAction.addMenu(menuMobile);
-		menuAction.addMenu(menuPC);
-		
-		menuAction.displyMenu();
-		menuAction.displaySubMenu(menuAction.getChoice(menuAction.getList().size()));
+		MenuAction menuActionLst= new ActionPerform().getInput();		
+		menuActionLst.displyMenu();
+		menuActionLst.displaySubMenu(menuActionLst.getChoice(menuActionLst.getList().size()));
 		
 	}
 
